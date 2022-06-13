c89fa24b316e848f5563b6273fd96766714c20f8
Chris Barnes <barnes.chris@gmail.com>
2022-06-12 21:37:19 -0400

    Add basic page structure and some initial styling

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+import "./Navigation.scss";
+
+export const Navigation = () => {
+  return (
+    <nav className="navigation">
+      <ul className="container">
+        <li>
+          <Link className="navigation-link navigation-link--home" to="/">
+            Great Reads
+          </Link>
+        </li>
+        <li>
+          <Link className="navigation-link" to="/add-new-author">
+            Add an Author
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
---
+++ src/pages/Home.jsx
@@ -0,0 +1,5 @@
+import { Layout } from "../components/Layout/Layout";
+
+export const Home = () => {
+  return <Layout>Home</Layout>;
+};
